39c2dfc9b2cc75f4df618248e1beaae9a2bd567e
Habibie Harits <harishabibie.hh@gmail.com>
2018-03-01 18:23:20 +0700

    exercise_3

---
+++ week2_exe3.js
@@ -0,0 +1,18 @@
+var Nama = prompt ('Masukan Nama');
+var peran = prompt ('Masukan peran');
+
+if((Nama === '')  && (peran === '')){
+  console.log('Nama harus diisi!');
+}else if((Nama === 'didi') && (peran === 'shinigami')) {
+  console.log('Selamat Datang di dunia DeathNote,');
+  console.log('Berikan buku DeathNote kepada kira');
+  console.log('Halo ' + '('+peran+')' + '' + Nama);
+}else if((Nama === 'Harits') && (peran === 'kira')) {
+  console.log('selamat Datang di dunia DeathNote');
+  console.log('Kamu adalah pembunuh keadilan');
+  console.log('Halo ' + '('+peran+')' + '' + Nama);
+}else if((Nama === 'dimitri') && (peran === 'L')){
+  console.log('Selamat Datang di dunia DeathNote');
+  console.log('Carilah kira, dan selamatkan dunia!');
+  console.log('Halo ' + '('+peran+')' + '' + Nama );
+} 
