27b8c3f3fb8dfab209e588c13c4ff1ffc660d610
Habibie Harits <36624345+HabibieHarits@users.noreply.github.com>
2018-03-02 11:07:49 +0700

    Update week2_exe3.js

--- week2_exe3.js
+++ week2_exe3.js
@@ -1,18 +1,18 @@
-var Nama = prompt ('Masukan Nama');
-var peran = prompt ('Masukan peran');
+var Nama = ''
+var peran = ''
 
 if((Nama === '')  && (peran === '')){
   console.log('Nama harus diisi!');
 }else if((Nama === 'didi') && (peran === 'shinigami')) {
-  console.log('Selamat Datang di dunia DeathNote,');
+  console.log('Selamat Datang di dunia DeathNote didi,');
   console.log('Berikan buku DeathNote kepada kira');
   console.log('Halo ' + '('+peran+')' + '' + Nama);
 }else if((Nama === 'Harits') && (peran === 'kira')) {
-  console.log('selamat Datang di dunia DeathNote');
+  console.log('selamat Datang di dunia DeathNote Harits');
   console.log('Kamu adalah pembunuh keadilan');
   console.log('Halo ' + '('+peran+')' + '' + Nama);
 }else if((Nama === 'dimitri') && (peran === 'L')){
-  console.log('Selamat Datang di dunia DeathNote');
+  console.log('Selamat Datang di dunia DeathNote dimitri');
   console.log('Carilah kira, dan selamatkan dunia!');
   console.log('Halo ' + '('+peran+')' + '' + Nama );
 } 
