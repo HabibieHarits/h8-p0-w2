ac460625c170dc48294e4e2471f78c55486c7ea1
Habibie Harits <36624345+HabibieHarits@users.noreply.github.com>
2018-03-08 14:19:51 +0700

    Update week3_exe4.js

--- week3_exe4.js
+++ week3_exe4.js
@@ -1,40 +1,62 @@
-function dataHandling2() {
+function dataHandling2(input){
+  
+  input.splice(1,2);
+  input.splice(1,0, 'Roman Alamsyah Elsharawy');
+  input.splice(2,0);
+  input.splice(2,0, 'Provinsi Bandar Lampung');
+  input.splice(4,0, 'Pria');
+  input.splice(5,1);
+  input.splice(5,0, 'SMA Internasional Metro');
+  console.log(input);
+  console.log('\n');
+  
+  var tanggal = input[3];
+  
+  console.log(tanggal)
 
-input.splice(1, 1, 'Roman Alamsyah Elsharawy');
-input.splice(2, 1, 'Provinsi Bandar Lampung');
-input.splice(4, 1, 'Pria', 'SMA International Metro');
-console.log(input);
+  Pisah_Tanggal = tanggal.split('/');
+  console.log(Pisah_Tanggal)
 
- var bulan = 05;
- switch(bulan) {
-   case 1:    {console.log('Januari'); break;}
-   case 2:    {console.log('Februari'); break;}
-   case 3:    {console.log('Maret'); break;}
-   case 4:    {console.log('April'); break;}
-   case 5:    {console.log('Mei'); break;}
-   case 6:    {console.log('Juni'); break;}
-   case 7:    {console.log('Juli'); break;}
-   case 8:    {console.log('Agustus'); break;}
-   case 9:    {console.log('September'); break;}
-   case 10:    {console.log('Oktober'); break;}
-   case 11:    {console.log('November'); break;}
-   case 12:    {console.log('Desember'); break;}
-   default:   {console.log('ngaco'); }
- }
+  potong_Tanggal = Pisah_Tanggal[1];
+  tanggal_lahir = parseInt(potong_Tanggal);
+  
+  var Bulan;
+  
+  switch(tanggal_lahir){
+    case 1: {Bulan=('Januari');break;}
+    case 2: {Bulan=('Februari');break;}
+    case 3: {Bulan=('Maret');break;}
+    case 4: {Bulan=('April');break;}
+    case 5: {Bulan=('Mei');break;}
+    case 6: {Bulan=('Juni');break;}
+    case 7: {Bulan=('Juli');break;}
+    case 8: {Bulan=('Agustus');break;}
+    case 9: {Bulan=('September');break;}
+    case 10: {Bulan=('Oktober');break;}
+    case 11: {Bulan=('November');break;}
+    case 12: {Bulan=('Desember');break;}
+  }
+  console.log(Bulan);
+  console.log();
+  
+  urutkan_Tanggal = Pisah_Tanggal.sort(function(a, b) { 
+    return b - a; 
+    
+  });
+  console.log(Pisah_Tanggal);
+  console.log();
+  
+  ubahTanggal = tanggal.split('/').join('-');
+  console.log(ubahTanggal);  
+  console.log();
 
-var format = input[3];
-var tanggal = format.split('/');
-tanggal.sort(function(value1, value2) { return value2 - value1 });
-console.log(tanggal);
+  
+  input.splice(1,2);
+  input.splice(1,0, 'Roman Alamsyah');
+  console.log(input[1]);
+    console.log();
 
-var joinVar = format.split('/');
-var join = joinVar.join('-');
-console.log(join);
 
-var nama = input[1];
-var i = nama.slice(0,15);
-console.log(i);
 }
 
-var input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"];
-dataHandling2(input);
+console.log(dataHandling2(["0001", "Roman Alamsyah", "Bandar Lampung", "21/06/1989", "Membaca"]));
