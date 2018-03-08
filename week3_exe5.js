f7dd9dcb80bff7b99f2f2cfa50dfd8116f576cd1
Habibie Harits <harishabibie.hh@gmail.com>
2018-03-08 15:14:18 +0700

    exe

---
+++ week3_exe5.js
@@ -0,0 +1,20 @@
+function palindrome(kata) {
+  // you can only write your code here!
+   var terbalik='';
+  for(var i=kata; i>=0; i--){
+    terbalik+=kata[i];
+    
+  }
+   if(kata===terbalik){
+    return true;
+  }else
+  return false;
+}
+
+
+// TEST CASES
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
